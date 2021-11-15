ba57342a8be24c8e81693af0a2387c188312ae71
Ildar Meyker <ildar.meyker@gmail.com>
2021-11-15 20:02:02 +0300

    Add progress.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 import "./modules/globals";
 
+import "bootstrap/js/dist/collapse";
 import "simplebar";
 
 import "./modules/Notify";
 import "./modules/SliderGallery";
+import "./modules/NavTabs";
 
 $(function () {
 	console.log("Hello, world!");
---
+++ src/js/modules/NavTabs.js
@@ -0,0 +1,38 @@
+const NavTabs = {
+	_handleTabClick(e) {
+		e.preventDefault();
+
+		const $root = $(e.currentTarget).closest(".nav-tabs");
+		const index = $(e.currentTarget).index();
+
+		this.renderView($root, index);
+		this.updateTarget($root, index);
+	},
+
+	renderView($root, index) {
+		$root
+			.find(".nav-tabs__item")
+			.removeClass("active")
+			.eq(index)
+			.addClass("active");
+	},
+
+	updateTarget($root, index) {
+		const target = $root.find(".nav-tabs__item").eq(index).attr("href");
+		$(target).siblings().removeClass("active").end().addClass("active");
+	},
+
+	init() {
+		$(document).on(
+			"click",
+			".js-nav-tabs__item",
+			this._handleTabClick.bind(this)
+		);
+	},
+};
+
+$(function () {
+	NavTabs.init();
+});
+
+export default NavTabs;
